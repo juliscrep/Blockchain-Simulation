1e04f49dab2955055be590760cf47adc0aebfc79
Raul Sanchez <fueguino84@gmail.com>
2023-08-04 00:10:29 -0300

    Change TransactionAbstract Creator

--- src/TransactionAbstract.js
+++ src/TransactionAbstract.js
@@ -1,15 +1,15 @@
 class TransactionAbstract{
-    
     constructor(Block) {
-        
         if (new.target === TransactionAbstract) {
             throw new Error('TransactionAbstract is an abstract class and could not be instantiated directly');
         }
-
-        this.Block=Block;
     }
 
-    getBlockAssociate(){
+    setBlockAssociate(Block) {
+        this.Block = Block;
+    }
+    
+    getBlockAssociate() {
         return this.Block.getId();
     }
 }
